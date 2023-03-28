f55c2614336c2e2f704e5c4a8fe24a8eaf30464a
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-03-28 17:06:11 +0300

    feat(app:components:note-card): make note-cards clickable links

--- src/app/components/note-card/note-card.module.ts
+++ src/app/components/note-card/note-card.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NoteCardComponent } from './note-card.component';
+import { RouterLinkWithHref } from '@angular/router';
 
 @NgModule({
   imports: [
+    RouterLinkWithHref
 
   ],
   declarations: [
