66b1c61a8db8de615c84a398c0ac3d96a2c04508
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-03-29 15:34:20 +0300

    feat(app:core:resolvers): redirect when noteId invalid

--- src/app/core/resolvers/note-id.resolver.ts
+++ src/app/core/resolvers/note-id.resolver.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 import { IRequest } from 'ngxs-requests-plugin';
-import { NotesService } from '../services/notes.service';
-import { filter, Observable } from 'rxjs';
+import { filter, Observable, tap } from 'rxjs';
 import { take } from 'rxjs/operators';
+import { NotesService } from '../services/notes.service';
 
 @Injectable({providedIn: 'root'})
 export class NoteByIdResolver implements Resolve<IRequest> {
-  constructor(private notesService: NotesService) {}
+  constructor(
+    private notesService: NotesService,
+    private router: Router,
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IRequest> {
-    this.notesService.getNoteById(Number(route.paramMap.get('id')));
+    this.notesService.getNoteById(parseInt(<string>route.paramMap.get('id')));
     return this.notesService.getNoteByIdRequestState$.pipe(
+      tap(res => {
+        if(res.status === 'fail') this.router.navigate(['notes/new'])
+      }),
       filter(res => res.loaded),
       take(1)
     )
