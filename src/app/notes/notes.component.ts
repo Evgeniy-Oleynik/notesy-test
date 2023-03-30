6ce696f3189712f84d8482dd3cc8227aa62f18f9
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-03-30 16:38:25 +0300

    feat(app:notes): remove unused code

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,15 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../core/services/auth.service';
-import { GetTopics } from '../core/ngxs/topics/topics.actions';
-import { Store } from '@ngxs/store';
-import { GetAllUsers } from '../core/ngxs/users/users.actions';
-import { UsersService } from '../core/services/users.service';
-import { Observable } from 'rxjs';
-import { User } from '../core/interfaces/user';
-import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
 import { NotesService } from '../core/services/notes.service';
-import { GetNotes } from '../core/ngxs/notes/notes.actions';
 
 @Component({
   selector: 'app-notes',
@@ -18,19 +10,14 @@ import { GetNotes } from '../core/ngxs/notes/notes.actions';
 })
 export class NotesComponent implements OnInit {
 
-  currentUser$ = this.authService.currentUser$;
-  userById$!: Observable<User>;
   notes$ = this.notesService.notes$;
-
-  idFormControl = new FormControl(1);
 
   constructor(
     private authService: AuthService,
-    private store: Store,
-    private usersService: UsersService,
     private router: Router,
     private notesService: NotesService,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
   }
@@ -41,18 +28,5 @@ export class NotesComponent implements OnInit {
 
   newNote() {
     this.router.navigate(['/notes/new']);
-  }
-
-  getNotes() {
-    this.store.dispatch(new GetNotes({userId: 56, topicId: 1}));
-  }
-
-  getUsers() {
-    this.store.dispatch(new GetAllUsers());
-  }
-
-  getUserById() {
-    this.usersService.getUserById(this.idFormControl.value as number);
-    this.userById$ = this.usersService.getUserById$(this.idFormControl.value as number);
   }
 }
