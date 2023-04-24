b32d0b104ea3c795820adf0ce3050b4b82f5a94e
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-04-24 12:39:14 +0300

    feat(app:notes): fix imports

--- src/app/notes/notes-routing.module.ts
+++ src/app/notes/notes-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotesComponent } from './notes.component';
-import { AuthGuard } from '../core/guard/auth.guard';
 import { TopicsResolver } from '../core/resolvers/topics.resolver';
 import { NoteByIdResolver } from '../core/resolvers/note-id.resolver';
 
@@ -9,16 +8,15 @@ const notesRoutes: Routes = [
   {
     path: '',
     component: NotesComponent,
+    resolve: [TopicsResolver],
   },
   {
     path: 'new',
-    canActivate: [AuthGuard],
     resolve: [TopicsResolver],
     loadChildren: () => import('./note-edit/note-edit.module').then(mod => mod.NoteEditModule)
   },
   {
     path: ':id',
-    canActivate: [AuthGuard],
     resolve: [TopicsResolver, NoteByIdResolver],
     loadChildren: () => import('./note-edit/note-edit.module').then(mod => mod.NoteEditModule)
   },
--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { NotesComponent } from './notes.component';
-import { NotesRoutingModule } from './notes-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+
 import { NoteCardModule } from '../components/note-card/note-card.module';
+import { NotesComponent } from './notes.component';
+import { NotesRoutingModule } from './notes-routing.module';
 
 @NgModule({
   imports: [
