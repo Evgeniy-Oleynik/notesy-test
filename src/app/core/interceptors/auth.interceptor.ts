08caa02c219b192c9712a741fa1e706e71163359
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-04-24 12:11:15 +0300

    feat(app:core:interceptors): make interceptor use state instead of local storage

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,20 +1,27 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LocalStorageService } from '../services/localstorage.service';
+import { filter, map, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 
 
 @Injectable()
 export class AuthorizationInterceptor implements HttpInterceptor {
 
   constructor(
-    private localStorageService: LocalStorageService,
+    private authService: AuthService,
     ) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    const token = this.localStorageService.getItem('authToken');
-    if (token) {
-      req = req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) });
-    }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token!: string;
+    this.authService.currentUser$.pipe(
+      filter(user => !!user.token),
+      take(1),
+      map(user => {
+        if (user.token) token = user.token
+      }),
+    ).subscribe();
+    req = req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) })
     return next.handle(req);
   }
 }
