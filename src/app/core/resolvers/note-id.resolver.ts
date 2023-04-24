d49572c52b5be7a288e84f51a94d01442c2f3b4f
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-04-24 12:15:57 +0300

    feat(app:core:resolvers): remove redirection function

--- src/app/core/resolvers/note-id.resolver.ts
+++ src/app/core/resolvers/note-id.resolver.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { IRequest } from 'ngxs-requests-plugin';
-import { filter, Observable, tap } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { NotesService } from '../services/notes.service';
 
@@ -9,17 +9,14 @@ import { NotesService } from '../services/notes.service';
 export class NoteByIdResolver implements Resolve<IRequest> {
   constructor(
     private notesService: NotesService,
-    private router: Router,
-  ) {}
+  ) {
+  }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IRequest> {
     this.notesService.getNoteById(parseInt(<string>route.paramMap.get('id')));
     return this.notesService.getNoteByIdRequestState$.pipe(
-      tap(res => {
-        if(res.status === 'fail') this.router.navigate(['notes/new'])
-      }),
       filter(res => res.loaded),
       take(1)
-    )
+    );
   }
 }
