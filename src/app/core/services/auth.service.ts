09b5d4bf9b68a6b83f8db1087bd2d5b123b08584
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-04-24 12:21:49 +0300

    feat(app:core:services): rework authorization service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
-import { GetUserByTokenRequestState, LogInUserRequestState, SignUpUserRequestState } from '../ngxs/auth/auth.state';
-import { Observable } from 'rxjs';
-import { User } from '../interfaces/user';
+import { filter, map, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { IRequest, RequestStatus } from 'ngxs-requests-plugin';
+import { LocalStorageService } from './local-storage.service';
+import { GetUserByTokenRequestState, LogInUserRequestState, LogOutUserRequestState, SignUpUserRequestState } from '../ngxs/auth/auth.state';
 import { AuthGetterState } from '../ngxs/auth/auth-getter.state';
-import { IRequest } from 'ngxs-requests-plugin';
 import { GetUserByToken, LogInUser, LogOutUser, SignUpUser } from '../ngxs/auth/auth.actions';
-import { LocalStorageService } from './localstorage.service';
+import { User } from '../../shared/interfaces/user';
 
 @Injectable({
   providedIn: 'root'
@@ -15,40 +17,70 @@ export class AuthService {
 
   @Select(SignUpUserRequestState)
   signUpUserRequestState$!: Observable<IRequest<User>>;
+
   @Select(AuthGetterState.authUser)
   currentUser$!: Observable<User>;
+
   @Select(LogInUserRequestState)
   logInUserRequestState$!: Observable<IRequest<User>>;
+
   @Select(GetUserByTokenRequestState)
   getUserByTokenRequestState$!: Observable<IRequest<User>>;
+
+  @Select(LogOutUserRequestState)
+  logOutUserRequestState$!: Observable<IRequest>;
 
   constructor(
     private store: Store,
     private localStorageService: LocalStorageService,
+    private router: Router,
   ) {
     this.getUserByToken();
   }
 
+  navigateAfterSuccess(request: Observable<IRequest>, navigateTo: string) {
+    request.pipe(
+      filter(res => res.loaded),
+      take(1),
+      map(res => {
+        if (res.status === RequestStatus.Success) {
+          this.router.navigate([navigateTo]);
+        }
+      })
+    ).subscribe();
+  }
+
   signUpUser(user: User) {
     this.store.dispatch(new SignUpUser(user));
+    this.navigateAfterSuccess(this.signUpUserRequestState$, 'notes');
   }
 
   logInUser(user: Partial<User>) {
     this.store.dispatch(new LogInUser(user));
+    this.navigateAfterSuccess(this.logInUserRequestState$, 'notes');
   }
 
   logOutUser() {
     this.store.dispatch(new LogOutUser());
+    this.navigateAfterSuccess(this.logOutUserRequestState$, 'login');
   }
 
   getUserByToken() {
-    const token = this.localStorageService.getItem('authToken');
+    const token = localStorage.getItem('notesy_authToken');
     if (token) {
+      console.log('local token');
       this.store.dispatch(new GetUserByToken(token));
     }
   }
 
   isAuthorized(): boolean {
-    return !!this.localStorageService.getItem('authToken');
+    let token: string | undefined;
+    this.currentUser$.pipe(
+      filter(user => !!user.token),
+      take(1),
+      map(user => token = user.token)
+    ).subscribe();
+    console.log('isAuthorized:', !!token);
+    return !!token;
   }
 }
