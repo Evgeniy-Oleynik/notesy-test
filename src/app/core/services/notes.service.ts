1d368d5a0b02de6cfe47f11a00f2c1f66057273c
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-01 12:19:43 +0300

    feat(app:core:services): add new variables

--- src/app/core/services/notes.service.ts
+++ src/app/core/services/notes.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
 import { IRequest } from 'ngxs-requests-plugin';
 import { Note } from '../../shared/interfaces/note';
 import { NotesGetterState } from '../ngxs/notes/notes-getter.state';
-import { DeleteNoteByIdRequestState, GetNoteByIdRequestState, GetNotesRequestState, PostNoteRequestState } from '../ngxs/notes/notes.state';
+import {
+  DeleteNoteByIdRequestState,
+  GetNoteByIdRequestState,
+  GetNotesRequestState,
+  PatchNoteByIdRequestState,
+  PostNoteRequestState
+} from '../ngxs/notes/notes.state';
 import { DeleteNoteById, GetNoteById, GetNotes, PatchNoteById, PostNote } from '../ngxs/notes/notes.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
+  currentNote$: BehaviorSubject<Note> = new BehaviorSubject<Note>({});
 
   @Select(NotesGetterState.getNotes)
   notes$!: Observable<Note[]>;
@@ -23,6 +30,9 @@ export class NotesService {
 
   @Select(PostNoteRequestState)
   postNoteRequestState$!: Observable<IRequest>;
+
+  @Select(PatchNoteByIdRequestState)
+  patchNoteByIdRequestState$!: Observable<IRequest>;
 
   @Select(DeleteNoteByIdRequestState)
   deleteNoteByIdRequestState$!: Observable<IRequest>;
