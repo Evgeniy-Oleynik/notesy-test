84672f48156b6c90ed38c88947254af1a4d57315
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-07 18:57:28 +0300

    feat(app:shared:interfaces): make data necessary

--- src/app/shared/interfaces/note.ts
+++ src/app/shared/interfaces/note.ts
@@ -1,10 +1,10 @@
 export interface Note {
-  id?: number | null;
-  userId?: number | null;
-  topicId?: number | null;
+  id: number | null;
+  userId: number | null;
+  topicId: number | null;
   topicType?: string | null;
-  title?: string | null;
-  text?: string | null;
+  title: string | null;
+  text: string | null;
   createdAt?: string | null;
   updatedAt?: string | null;
 }
