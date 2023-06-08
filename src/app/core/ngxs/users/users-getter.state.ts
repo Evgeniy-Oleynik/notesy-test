4f998d8dd3a006ef7e03e4187c74be731f01b5ce
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-08 18:53:56 +0300

    feat(app:core:ngxs:users): fix users getter

--- src/app/core/ngxs/users/users-getter.state.ts
+++ src/app/core/ngxs/users/users-getter.state.ts
@@ -4,12 +4,11 @@ import { UsersState, UsersStateModel } from './users.state';
 export class UsersGetterState {
   @Selector([UsersState])
   static getAllUsers(state: UsersStateModel) {
-    return state.entities;
+    return state.ids.map(id => state.entities[id]);
   }
 
   static getUser(id: number) {
     return createSelector([UsersState], (state: UsersStateModel) => {
-      console.log(id);
       return state.entities[id];
     })
   }
