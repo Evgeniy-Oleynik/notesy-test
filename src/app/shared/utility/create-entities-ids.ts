2265f67bef4ccdca698397d59c6250704f08c52f
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-08 18:48:34 +0300

    feat(app:shared:utility): make new entities every time

--- src/app/shared/utility/create-entities-ids.ts
+++ src/app/shared/utility/create-entities-ids.ts
@@ -2,7 +2,7 @@ export function createEntitiesIds(state: any, array: any[], keyName: string = 'i
   return array.reduce((acc, item) => {
     return {
       entities: {...acc.entities, [item[keyName]]: item},
-      ids: state.ids.indexOf(item[keyName]) === -1 && acc.ids.indexOf(item[keyName]) === -1 ? [...acc.ids, item[keyName]] : acc.ids,
+      ids: [...acc.ids, item[keyName]],
     };
-  }, {entities: {...state.entities}, ids: [...state.ids]});
+  }, {entities : {}, ids: []});
 }
