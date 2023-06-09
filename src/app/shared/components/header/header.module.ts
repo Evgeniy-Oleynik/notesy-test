1c4d826b7926c4fb482cb9a5fd5b544b014a64dd
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-09 16:55:34 +0300

    feat(app:shared): move components to shared folder

--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLinkWithHref } from '@angular/router';
 import { HeaderComponent } from './header.component';
-import { SharedModule } from '../../shared/shared.module';
+import { SharedModule } from '../../shared.module';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 
