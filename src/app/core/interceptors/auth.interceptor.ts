4d378ded85ef658e7a44a58e73536035dc3fc904
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-14 15:29:47 +0300

    feat(app:core:interceptors): fix log out errors

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { filter, map, Observable, take } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
 
@@ -15,15 +15,12 @@ export class AuthorizationInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let token!: string;
-    this.authService.currentUser$.pipe(
-      filter(user => !!user.token),
-      take(1),
-      map(user => {
-        if (user.token) {
-          token = user.token;
-        }
-      }),
-    ).subscribe();
+    const currentUserSubscription$ = this.authService.currentUser$.pipe(
+      filter(user => !!user?.token),
+    ).subscribe(user => {
+      token = user.token;
+    });
+    currentUserSubscription$.unsubscribe();
     req = req.clone({headers: req.headers.set('Authorization', `Bearer ${token}`)});
     return next.handle(req);
   }
