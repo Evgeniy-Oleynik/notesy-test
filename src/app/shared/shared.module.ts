69018ca8658417bbead2a4307cf239f667f49609
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-15 18:37:44 +0300

    feat(app:shared): add new shared module import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
@@ -21,6 +22,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatMenuModule,
     MatSelectModule,
+    MatTooltipModule,
     ReactiveFormsModule,
   ],
   providers: [],
@@ -34,6 +36,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatMenuModule,
     MatSelectModule,
+    MatTooltipModule,
     ReactiveFormsModule,
   ],
   bootstrap: []
