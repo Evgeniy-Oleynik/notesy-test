c5ce3bd44bdc8716d4b6a2f097143939e2b7b804
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-19 19:32:00 +0300

    feat(app): import spinner module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { HeaderModule } from './shared/components/header/header.module';
+import { SpinnerModule } from './shared/components/spinner/spinner.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { HeaderModule } from './shared/components/header/header.module';
     AppRoutingModule,
     CoreModule,
     HeaderModule,
+    SpinnerModule,
   ],
   providers: [],
   exports: [
