d03eb519d807a3524c3962f58a116c800cf11a68
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-20 16:25:00 +0300

    feat(app:signup): update layout

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 import { AuthService } from '../core/services/auth.service';
 
@@ -13,9 +13,10 @@ export class SignupComponent implements OnInit {
 
   signUpForm = new FormGroup<any>({
     name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required])
-  });
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirm: new FormControl('', [Validators.required])
+  }, {validators: [this.passwordsAreNotEqual('password', 'confirm')]});
 
   constructor(
     private authService: AuthService,
@@ -23,14 +24,33 @@ export class SignupComponent implements OnInit {
   ) {
   }
 
-  ngOnInit(): void {
+  get nameFormControl() {
+    return this.signUpForm.get('name') as FormControl;
+  }
+  get emailFormControl() {
+    return this.signUpForm.get('email') as FormControl;
+  }
+  get passwordFormControl() {
+    return this.signUpForm.get('password') as FormControl;
+  }
+  get confirmFormControl() {
+    return this.signUpForm.get('confirm') as FormControl;
   }
 
-  logIn() {
-    this.router.navigate(['/login']);
+  ngOnInit(): void {
   }
 
   signUp() {
     this.authService.signUpUser(this.signUpForm.value);
+  }
+
+  passwordsAreNotEqual(password, confirm) {
+    return (control: AbstractControl) => {
+      const firstControl = control.get(password);
+      const secondControl = control.get(confirm);
+      if (secondControl.value && (firstControl.value !== secondControl.value)) {
+        return this.confirmFormControl.setErrors({notEqual: true})
+      }
+    }
   }
 }
