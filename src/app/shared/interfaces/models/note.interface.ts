b893f02e06bd8d4625c4a63921bdd5bec436a896
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-23 13:18:29 +0300

    feat(app:shared:interfaces:models): remove unused code

--- src/app/shared/interfaces/models/note.interface.ts
+++ src/app/shared/interfaces/models/note.interface.ts
@@ -1,11 +1,11 @@
 export interface Note {
-  id: number | null;
-  userId: number | null;
-  userName?: string | null;
-  topicId: number | null;
-  topicType?: string | null;
-  title: string | null;
-  text: string | null;
-  createdAt?: string | null;
-  updatedAt?: string | null;
+  id: number;
+  userId: number;
+  userName?: string;
+  topicId: number;
+  topicType?: string;
+  title: string;
+  text: string;
+  createdAt?: string;
+  updatedAt?: string;
 }
