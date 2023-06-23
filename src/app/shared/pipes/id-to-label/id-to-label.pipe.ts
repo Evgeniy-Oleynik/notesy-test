48981701b8f45d3747160b4ad119d4f71e54af8b
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-23 12:09:35 +0300

    feat(app:shared:pipes:id-to-label): add generic type

--- src/app/shared/pipes/id-to-label/id-to-label.pipe.ts
+++ src/app/shared/pipes/id-to-label/id-to-label.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'idToLabel'})
 export class IdToLabelPipe implements PipeTransform {
 
-  transform(id: number, arr: any[], label: string ): string {
-    return arr.find(item => item.id === id)[label]
+  transform<T extends {id?: number}>(id: number, array: T[], label: string ): string {
+    return array.find(item => item.id === id)[label]
   }
 }
