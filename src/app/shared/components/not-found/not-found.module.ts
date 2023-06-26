bc2d671a5cc314ec107c7cacb0cc6d3261fc8fe5
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-26 17:29:13 +0300

    feat(app:shared:components:not-found): update page 404

--- src/app/shared/components/not-found/not-found.module.ts
+++ src/app/shared/components/not-found/not-found.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../../shared.module';
+
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './not-found.component';
 
 @NgModule({
   imports: [
-    NotFoundRoutingModule
+    NotFoundRoutingModule,
+    SharedModule
   ],
   declarations: [
     NotFoundComponent
