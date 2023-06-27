b4f7c98c07f28959ac874b239685e1d17148c6db
Evgen Oliynik <evgeniy.o@2muchcoffee.com>
2023-06-27 12:56:03 +0300

    feat(app:core:guard): update authorization guard

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
-import { map } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { RequestStatus } from 'ngxs-requests-plugin';
+import { map, of, switchMap } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class AuthGuard {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
     private router: Router,
+    private localStorageService: LocalStorageService,
   ) {
   }
 
@@ -20,8 +23,42 @@ export class AuthGuard {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ) {
+    return this.guard();
+  }
+
+  canLoad(
+    route: Route, segments: UrlSegment[]
+  ) {
+    return this.guard();
+  }
+
+  private guard() {
     return this.authService.currentUser$.pipe(
-      map(user => user?.token ? true : this.router.navigate(['login'])),
+      map(user => !!user?.token),
+      switchMap((isToken) => {
+        const tokenFromLS = this.localStorageService.getItem('authToken');
+
+        switch (true) {
+          case (isToken): {
+            return of(true);
+          }
+
+          case (!!tokenFromLS): {
+            return this.authService.getUserByToken().pipe(
+              map(res => res.status === RequestStatus.Success),
+              switchMap(isSuccess => {
+                return isSuccess ? of(true)
+                  : this.router.navigate(['login']);
+              })
+            );
+          }
+
+          default: {
+            this.router.navigate(['login']);
+            return of(false);
+          }
+        }
+      })
     );
   }
 }
